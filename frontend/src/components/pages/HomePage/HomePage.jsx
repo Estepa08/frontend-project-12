bb175ae4f6667f6348ea844857d7f5507c65cf02
Evgeny_Stepanov <step.evgeny@gmail.com>
2026-07-02 17:51:54 +0300

    fix: add Регистрация button text on HomePage

--- frontend/src/components/pages/HomePage/HomePage.jsx
+++ frontend/src/components/pages/HomePage/HomePage.jsx
@@ -37,7 +37,7 @@ const HomePage = () => {
 
             <Link to="/signup" className={styles.fullWidth}>
               <Button icon={<UserAddOutlined />} block size="large" className={styles.button}>
-                Создать аккаунт
+                Регистрация
               </Button>
             </Link>
 
