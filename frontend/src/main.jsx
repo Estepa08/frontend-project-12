90c6627ac70e04589152a43bb281b4e41f122ed7
Evgeny_Stepanov <step.evgeny@gmail.com>
2026-07-03 12:39:41 +0300

    fix: move Bootstrap JS import to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './slices/index.js';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import './index.css';
 import './i18n.js';
 
